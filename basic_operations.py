b56ad96d700b4a803890e8b6f00419a3cfd2b018
edwardalx <eobkwaku@gmail.com>
2024-12-10 11:32:28 +0000

    BasicArithmeticExercise

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1=10
+number2=5
+sum = number1 + number2
+difference = number1 - number2
+product = number1*number2
+#[operation] of [number1] and [number2] is [result].
+print("sum of", number1, "and", number2, "is", sum)
+print("difference of", number1, "and", number2, "is", difference)
+print("product of", number1, "and", number2, "is", product)

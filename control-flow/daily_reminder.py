2bbd07aff44d7faea446d0b65736b9d452241b49
edwardalx <eobkwaku@gmail.com>
2024-12-18 10:44:23 +0000

    fix

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,9 +1,9 @@
-task = input("Enter task description: ")
-priority = input("Enter task priority (high, medium, low): ")
-time_bound = input("Is the task is time-bound (yes or no): ")
-match time_bound:
+Task = input("Enter task description: ")
+Priority = input("Enter task priority (high, medium, low): ")
+Time_bound = input("Is the task is time-bound (yes or no): ")
+match Time_bound:
     case "yes":
-        print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
+        print(f"Reminder: {Task} is a {Priority} priority task that requires immediate attention today!")
     case _ if "no":
-        print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+        print(f"Note: {Task} is a {Priority} priority task. Consider completing it when you have free time.")
    

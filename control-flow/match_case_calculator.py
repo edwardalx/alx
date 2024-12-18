b98f6d68ad3cc9b84865b485f2f5535d9e479154
edwardalx <eobkwaku@gmail.com>
2024-12-18 00:35:12 +0000

    fixForweatherApp

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,23 +1,23 @@
-num1 = float(input("Enter the first number:"))
-num2 = float(input("Enter the second number:"))
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
 operation = input("Choose the operation (+, -, *, /):")
 
 match operation:
     case _ if operation == "+":
         result = num1 + num2
-        print (f"The result is {result}")
+        print (f"The result is {result}.")
     case _ if operation == "-":
         result = num1 - num2
-        print (f"The result is {result}")
+        print (f"The result is {result}.")
     case _ if operation == "*":
         result = num1 * num2
-        print ("The result is", {result})
+        print (f"The result is {result}.")
     case _ if operation == "/":
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
             result = num1 / num2
-            print (f"The result is {result}")
+            print (f"The result is {result}.")
        
     case _:
         print("Please enter (+, -, *, /) in the operation")

cc969b780235b6a9ff6b2209639e9b28f16516be
edwardalx <eobkwaku@gmail.com>
2024-12-18 09:23:29 +0000

    DrawingPatternswithNestedLoop

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the size of the pattern: "))
+i = 0 
+while i < a:
+    i+=1
+    j=0
+    while j < a:
+        j = j+1
+      # print(f"{i:4}*{j}", end="")
+        print("*", end = "")
+    print()

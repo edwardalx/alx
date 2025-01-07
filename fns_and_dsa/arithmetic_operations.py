05df347ad1f9a4ba66bc41d0c6cc9d6363244d3e
Edward Osei-Bonsu <Edward.Osei-Bonsu@sharpgaming.onmicrosoft.com>
2025-01-07 12:43:14 +0000

    v1

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+# operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+def perform_operation(num1,num2,operation):
+    if operation == "add":
+        result = num1 + num2
+        return result
+    elif operation == "subtract":
+        result = num1 - num2
+        return result  
+    elif operation == "multiply":
+        result = num1 * num2
+        return result
+    elif operation == "divide":
+        result = num1 / num2  
+        return result
+    
+# print(perform_operation(num1,num2,operation))

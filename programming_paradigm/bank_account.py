0aed0c54dd11a250815243098ff8644f66b3a0ec
Edward Osei-Bonsu <Edward.Osei-Bonsu@sharpgaming.onmicrosoft.com>
2025-01-15 00:17:32 +0000

    wwek6_v5

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -11,10 +11,14 @@ class BankAccount:
     def withdraw(self,amount):
         if amount > self.account_balance:
             return  f"Insufficient funds."
-        self.account_balance -= amount
+        self.account_balance = self.account_balance - amount
         return f"You've withdrawn {amount} and your new balance {self.account_balance}"
     
     def display_balance(self):
         result = ["Current Balance:"]
         return print(f"{result[0]} is {self.account_balance}")
+    
+account = BankAccount(200)  # Assume an initial balance of 200
+print(account.withdraw(150))  # Output: You've withdrawn 150 and your new balance is 50
+print(account.withdraw(100))  # Output: Insufficient funds.
 
